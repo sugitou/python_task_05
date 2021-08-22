eef03eacc2282d36e18660190f1da37f83c8396f
sugitou <sugi_tou108@yahoo.co.jp>
2021-08-22 16:06:51 +0900

    課題5完了

--- possys.py
+++ possys.py
@@ -1,9 +1,9 @@
 import os
 import csv
 import datetime
+import random
 
 item_csv = 'item.csv'
-# total_price = 0
 ### 商品クラス
 class Item:
     def __init__(self,item_code,item_name,price):
@@ -36,7 +36,7 @@ class Order:
         try:
             item_code_i = int(item_code)
         except ValueError:
-            return '商品登録からやり直してください。\n', 0
+            return '商品登録からやり直してください。\n'
 
         item_code_s = str(item_code_i).zfill(3)
         item_num_i = 0
@@ -46,7 +46,7 @@ class Order:
             try:
                 item_num_i = int(item_num)
             except ValueError:
-                return '商品登録からやり直してください。\n', 0
+                return '商品登録からやり直してください。\n'
             
             item_code_num = {'code':item_code_s, 'num':item_num_i}#
             self.item_order_list.append(item_code_num)#
@@ -67,36 +67,71 @@ class Order:
             return goods_print
         else:
             # 商品コードが存在しない場合
-            return f'{item_code_s}の商品は存在しません。\n', 0
+            return f'{item_code_s}の商品は存在しません。\n'
     
     def get_price(self):
         return self.total_price
 
+### ファイル操作クラス
+class Option:
+    @staticmethod
+    def read_csv(filename):
+        item_master = []
+        # csvから商品登録
+        with open(f'./{filename}', 'r', encoding='utf_8-sig') as f:
+            h = next(csv.reader(f))
+            for row in f:
+                rows = row.rstrip().split(',')
+                item_master.append(Item(rows[0], rows[1], rows[2]))
+        return item_master
 
-
-    
-### メイン処理
+### 商品登録
 def regist(code, number, order):
     # 出力値を受け取る
     output_data = order.add_item_order(code, number)
     str_p = f'総額： ￥{order.get_price()}\n'
     output_data += str_p
     return output_data
-
-def set_price(order):
-    return order.get_price()
-
-def payment(money, order):
-    print("計算するよ。")
+### 総額
+# def set_price(order):
+#     return order.get_price()
+### クーポン発券
+def cp_gift():
+    cps = ['Lucky10%', 'Lucky20%', 'Lucky30%']
+    gift = random.choice(cps)
+    if gift == 'Lucky10%':
+        cp_result = f'10%引きのクーポンが発券されました！\n{gift}'
+    elif gift == 'Lucky20%':
+        cp_result = f'20%引きのクーポンが発券されました！\n{gift}'
+    else:
+        cp_result = f'30%引きのクーポンが発券されました！\n{gift}'
+    return cp_result
+### クーポン交換
+def cp_trans(tp, ticket):
+    if ticket == 'Lucky10%':
+        disc_amount = tp * 0.9
+    elif ticket == 'Lucky20%':
+        disc_amount = tp * 0.8
+    elif ticket == 'Lucky30%':
+        disc_amount = tp * 0.7
+    else:
+        disc_amount = tp
+    return int(disc_amount)
+### レジ計算
+def payment(money, coupon, order):
     money_i = 0
     try:
         money_i = int(money)
     except ValueError:
         return '数字を入力してください。'
-    if money_i > order.get_price():
-        change = money_i - order.get_price()
-        change_result = f'{change}円のお返しです。\nまたのお越しをお待ちしております！'
+    # クーポン適用
+    disc_after = cp_trans(order.get_price(), coupon)
+    # お預かり金額の確認
+    if money_i >= disc_after:
+        change = money_i - disc_after
+        cp = cp_gift()
+        change_result = f'{change}円のお返しです。\n{cp}\nまたのお越しをお待ちしております。\n'
         return change_result
     else:
-        return '商品を購入することができません。'
+        return 'お金が足りません。'
     
--- view.py
+++ view.py
@@ -1,42 +1,24 @@
-import csv
 import eel
 import desktop
 import possys
 
 app_name="web"
 end_point="index.html"
-size=(700,600)
+size=(600,700)
 item_csv = 'item.csv'
-# total_price = 0
-
-### ファイル操作クラス
-class Option:
-    @staticmethod
-    def read_csv(filename):
-        item_master = []
-        # csvから商品登録
-        with open(f'./{filename}', 'r', encoding='utf_8-sig') as f:
-            h = next(csv.reader(f))
-            for row in f:
-                rows = row.rstrip().split(',')
-                item_master.append(possys.Item(rows[0], rows[1], rows[2]))
-        return item_master
 
 @ eel.expose
 def pos_system(code, number):
     output_data = possys.regist(code, number, order)
     return output_data
-    
 @ eel.expose
-def pay_system(money):
-    print("関数内です。")
-    change_data = possys.payment(money, order)
+def pay_system(money, coupon):
+    change_data = possys.payment(money, coupon, order)
     return change_data
-
 
 # マスタ登録
 # csvから商品登録
-option = Option()
+option = possys.Option()
 item_master = option.read_csv(item_csv)
 # オーダー登録
 order=possys.Order(item_master)
